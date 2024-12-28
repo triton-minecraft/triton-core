a64ac4f1e0fb031ab918b9e1cee7a3c0f2969b4c
wiji <wijigaming.yt@gmail.com>
2024-12-28 00:34:50 -0700

    fix gradle shenanigans

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "dev.kyriji"
@@ -11,4 +12,27 @@ repositories {
 
 dependencies {
     implementation(project(":common"))
+    implementation("net.minestom:minestom-snapshots:9803f2bfe3")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Minestom has a minimum Java version of 21
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "org.example.Main" // Change this to your main class
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("") // Prevent the -all suffix on the shadowjar file.
+    }
 }
