a4f0610052a6047bdaec114765bc448a49e29424
wiji <wijigaming.yt@gmail.com>
2025-01-10 23:07:05 -0700

    fix velocity

--- common/src/main/java/dev/kyriji/common/models/TritonHook.java
+++ common/src/main/java/dev/kyriji/common/models/TritonHook.java
@@ -1,10 +1,5 @@
 package dev.kyriji.common.models;
 
-import dev.kyriji.common.commands.models.TritonCommand;
-import dev.kyriji.common.enums.TritonFeature;
-
-import java.util.Map;
-
 public interface TritonHook {
 
 }
---
+++ velocity/src/main/java/dev/kyriji/velocity/controllers/ConfigManager.java
@@ -0,0 +1,44 @@
+package dev.kyriji.velocity.controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ConfigManager {
+	private static final String CONFIG_RESOURCE = "/config.json";
+	private static JsonObject config;
+	private static final Gson gson = new Gson();
+
+	public static void init() {
+		loadConfig();
+	}
+
+	private static void loadConfig() {
+		try (InputStream is = ConfigManager.class.getResourceAsStream(CONFIG_RESOURCE)) {
+			if (is == null) {
+				throw new IllegalStateException("Config resource not found: " + CONFIG_RESOURCE);
+			}
+
+			try (JsonReader reader = new JsonReader(new InputStreamReader(is))) {
+				config = gson.fromJson(reader, JsonObject.class);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to load config from resources", e);
+		}
+	}
+
+	public static String getValue(String key) {
+		if (config == null) {
+			init();
+		}
+
+		if (config.has(key)) {
+			return config.get(key).getAsString();
+		}
+		return null;
+	}
+}
