242ff9aaa9a95310f386a13ea44b2bbce1d16464
wiji <wijigaming.yt@gmail.com>
2025-01-18 22:12:43 -0800

    move velocity enable to minestom init

--- minestom/src/main/java/dev/kyriji/minestom/TritonCoreMinestom.java
+++ minestom/src/main/java/dev/kyriji/minestom/TritonCoreMinestom.java
@@ -1,6 +1,8 @@
 package dev.kyriji.minestom;
 
 import dev.kyriji.common.TritonCoreCommon;
+import dev.kyriji.common.config.documents.CoreConfig;
+import dev.kyriji.common.config.enums.ConfigType;
 import dev.kyriji.minestom.controllers.ConfigManager;
 import dev.kyriji.minestom.hooks.*;
 import dev.kyriji.minestom.models.LuckpermsAdapter;
@@ -8,10 +10,13 @@ import me.lucko.luckperms.common.config.generic.adapter.MultiConfigurationAdapte
 import me.lucko.luckperms.minestom.CommandRegistry;
 import me.lucko.luckperms.minestom.LuckPermsMinestom;
 import net.luckperms.api.LuckPerms;
+import net.minestom.server.extras.velocity.VelocityProxy;
 
 import java.nio.file.Path;
 
 public class TritonCoreMinestom {
+	public static TritonCoreCommon core;
+	public static LuckPerms luckPerms;
 
 	public static void main(String[] args) {
 		init();
@@ -20,7 +25,7 @@ public class TritonCoreMinestom {
 	public static void init() {
 		ConfigManager.init();
 
-		TritonCoreCommon core = TritonCoreCommon.builder()
+		core = TritonCoreCommon.builder()
 				.withConfig(new MinestomConfigHook())
 				.withCommands(new MinestomCommandHook())
 				.withInventory(new MinestomInventoryHook())
@@ -30,13 +35,18 @@ public class TritonCoreMinestom {
 
 		Path directory = Path.of("luckperms");
 
-		LuckPerms luckPerms = LuckPermsMinestom.builder(directory)
+		luckPerms = LuckPermsMinestom.builder(directory)
 				.commandRegistry(CommandRegistry.minestom())
 				.configurationAdapter(plugin -> new MultiConfigurationAdapter(plugin,
 						new LuckpermsAdapter(plugin)
 				)).dependencyManager(true)
 				.enable();
 
+		CoreConfig coreConfig = dev.kyriji.common.config.controllers.ConfigManager.getConfig(ConfigType.CORE);
+		if(coreConfig == null) throw new NullPointerException("Core config not found");
+
+		if(System.getenv("ENV") != null && System.getenv("ENV").equals("prod"))
+			VelocityProxy.enable(coreConfig.getVelocitySecret());
 	}
 
 
