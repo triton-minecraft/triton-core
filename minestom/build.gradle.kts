55f894ff5de63e9075702df5c4e4d94ce9a39911
Timothy <timothyrwashburn@gmail.com>
2025-01-18 22:27:26 -0800

    add local publishing

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("maven-publish")
 }
 
 group = "dev.kyriji"
-version = ""
+version = "0.0.0"
 
 repositories {
     mavenCentral()
@@ -43,5 +44,17 @@ tasks {
     shadowJar {
         mergeServiceFiles()
         archiveClassifier.set("")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("shadow") {
+            project.shadow.component(this)
+
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
     }
 }
