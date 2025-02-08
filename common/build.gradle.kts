5f606e69f5c69ac4b3a11a43f6057526c9203860
wiji <wijigaming.yt@gmail.com>
2025-02-07 18:14:38 -0800

    update bmc api version

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.big-minecraft:bmc-api:97074e9964")
+    compileOnly("com.github.big-minecraft:bmc-api:7563425c0b")
     compileOnly("org.mongodb:mongodb-driver-legacy:4.5.0")
     compileOnly("net.luckperms:api:5.4")
     compileOnly("com.google.code.gson:gson:2.8.8")
